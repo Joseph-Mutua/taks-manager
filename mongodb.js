57035686ffda5d23999dbffd115ecba600b0643a
Joseph Mutua <Mutuaj793@gmail.com>
2020-07-28 22:18:01 +0300

    Initial commit

---
+++ mongodb.js
@@ -0,0 +1,22 @@
+//CRUD Create Read Update
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database!");
+    }
+    const db = client.db(databaseName);
+    db.collection("users").insertOne({
+      name: "Mutua",
+      age: 22,
+    });
+  }
+);
