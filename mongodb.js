b5cf2d3d75c3a9597c7d31d5d06da979ecd281f9
Joseph Mutua <Mutuaj793@gmail.com>
2020-07-29 00:01:34 +0300

    Add query for a single user

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,6 @@
 //CRUD Create Read Update
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -19,60 +18,11 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "Mutua",
-    //     age: 22,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert User");
-    //     }
-
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection("users").insertMany(
-    //   [
-    //     {
-    //       name: "Jen",
-    //       age: 28,
-    //     },
-    //     {
-    //       name: "Kamwana",
-    //       age: 24,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents!");
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Do Laundry",
-          completed: true,
-        },
-        {
-          description: "Daily Reading",
-          completed: true,
-        },
-        {
-          description: "Daily workout",
-          completed: false,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("Tasks Not Inserted!");
-        }
-        console.log(result.ops);
+    db.collection("users").findOne({ name: "Mutua" }, (error, user) => {
+      if (error) {
+        console.log("Unable to fetch!");
       }
-    );
+      console.log(user);
+    });
   }
 );
