7b2c6b7dce2fe4c2093681a8d4c5fc6a20364e4e
Joseph Mutua <Mutuaj793@gmail.com>
2020-07-28 22:57:21 +0300

    Add users to MongoDB database

--- mongodb.js
+++ mongodb.js
@@ -8,15 +8,51 @@ const databaseName = "task-manager";
 
 MongoClient.connect(
   connectionURL,
-  { useNewUrlParser: true },
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
   (error, client) => {
     if (error) {
       return console.log("Unable to connect to database!");
     }
+
     const db = client.db(databaseName);
-    db.collection("users").insertOne({
-      name: "Mutua",
-      age: 22,
-    });
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Mutua",
+    //     age: 22,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert User");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jen",
+    //       age: 28,
+    //     },
+    //     {
+    //       name: "Kamwana",
+    //       age: 24,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents!");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    
+
   }
 );
