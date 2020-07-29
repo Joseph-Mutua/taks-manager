d2405cf932c35b11ee0a6844f27d8975b3408bd7
Joseph Mutua <Mutuaj793@gmail.com>
2020-07-29 15:04:31 +0300

    Queries to Delete multiple documnets

--- mongodb.js
+++ mongodb.js
@@ -18,59 +18,87 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").findOne({ name: "Mutua" }, (error, user) => {
-    //   if (error) {
-    //     console.log("Unable to fetch!");
-    //   }
-    //   console.log(user);
-    // });
+    //READ DATABASE COLLECTIONS
 
-    // db.collection("users")
-    //   .find({ age: 22 })
-    //   .toArray((error, users) => {
-    //     console.log(users);
-    //   });
+      db.collection("users").findOne({ name: "Mutua" }, (error, user) => {
+        if (error) {
+          console.log("Unable to fetch!");
+        }
+        console.log(user);
+      });
 
-    // db.collection("users")
-    //   .find({ age: 22 })
-    //   .count((error, count) => {
-    //     console.log(count);
-    //   });
+      db.collection("users")
+        .find({ age: 22 })
+        .toArray((error, users) => {
+          console.log(users);
+        });
 
-    // db.collection("tasks").findOne(
-    //   { _id: new ObjectID("5f2086424f0407515d236f89") },
-    //   (error, task) => {
-    //     if (error) {
-    //       return console.log("Unable to Fetch!");
-    //     }
-    //     console.log(task);
-    //   }
-    // );
+      db.collection("users")
+        .find({ age: 22 })
+        .count((error, count) => {
+          console.log(count);
+        });
 
-    // db.collection("tasks")
-    //   .find({ completed: false })
-    //   .toArray((error, tasks) => {
-    //     console.log(tasks);
-    //   });
+      db.collection("tasks").findOne(
+        { _id: new ObjectID("5f2086424f0407515d236f89") },
+        (error, task) => {
+          if (error) {
+            return console.log("Unable to Fetch!");
+          }
+          console.log(task);
+        }
+      );
 
-    // db.collection("users")
-    //   .updateOne(
-    //     { _id: new ObjectID("5f20767620b3bf4828aa3ab3") },
-    //     {
-    //       $inc: {
-    //         age: 1,
-    //       },
-    //     }
-    //   )
-    //   .then((result) => {
-    //     console.log(result);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+      db.collection("tasks")
+        .find({ completed: false })
+        .toArray((error, tasks) => {
+          console.log(tasks);
+        });
+
+    //UPDATE DATABASE DOCUMENTS
+
+      db.collection("users")
+        .updateOne(
+          { _id: new ObjectID("5f20767620b3bf4828aa3ab3") },
+          {
+            $inc: {
+              age: 1,
+            },
+          }
+        )
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      db.collection("tasks")
+        .updateMany({ completed: true }, { $set: { completed: false } })
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+    //DELETE COLLECTIONS
+
+    db.collection("users")
+      .deleteMany({
+        age: 28,
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
     db.collection("tasks")
-      .updateMany({ completed: true }, { $set: { completed: false } })
+      .deleteOne({
+        _id: new ObjectID("5f208700a5c02951953565b3"),
+      })
       .then((result) => {
         console.log(result);
       })
