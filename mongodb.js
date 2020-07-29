d0ee2e2579f7019518a5fe81263f58dca500b9b0
Joseph Mutua <Mutuaj793@gmail.com>
2020-07-29 15:28:17 +0300

    Finish CRUD operations on database

--- mongodb.js
+++ mongodb.js
@@ -18,69 +18,103 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
+    //CREATE DATABASE DOCUMENTS
+
+    db.collection("users").insertOne(
+      {
+        name: "Andrew",
+        age: 27,
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user!");
+        }
+        console.log(result.ops);
+      }
+    );
+
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Kuria",
+          age: 23,
+        },
+        {
+          name: "Gunther",
+          age: 37,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert User!");
+        }
+        console.log(result.ops);
+      }
+    );
+
     //READ DATABASE COLLECTIONS
 
-      db.collection("users").findOne({ name: "Mutua" }, (error, user) => {
-        if (error) {
-          console.log("Unable to fetch!");
-        }
-        console.log(user);
+    db.collection("users").findOne({ name: "Mutua" }, (error, user) => {
+      if (error) {
+        console.log("Unable to fetch!");
+      }
+      console.log(user);
+    });
+
+    db.collection("users")
+      .find({ age: 22 })
+      .toArray((error, users) => {
+        console.log(users);
       });
 
-      db.collection("users")
-        .find({ age: 22 })
-        .toArray((error, users) => {
-          console.log(users);
-        });
+    db.collection("users")
+      .find({ age: 22 })
+      .count((error, count) => {
+        console.log(count);
+      });
 
-      db.collection("users")
-        .find({ age: 22 })
-        .count((error, count) => {
-          console.log(count);
-        });
-
-      db.collection("tasks").findOne(
-        { _id: new ObjectID("5f2086424f0407515d236f89") },
-        (error, task) => {
-          if (error) {
-            return console.log("Unable to Fetch!");
-          }
-          console.log(task);
+    db.collection("tasks").findOne(
+      { _id: new ObjectID("5f2086424f0407515d236f89") },
+      (error, task) => {
+        if (error) {
+          return console.log("Unable to Fetch!");
         }
-      );
+        console.log(task);
+      }
+    );
 
-      db.collection("tasks")
-        .find({ completed: false })
-        .toArray((error, tasks) => {
-          console.log(tasks);
-        });
+    db.collection("tasks")
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        console.log(tasks);
+      });
 
     //UPDATE DATABASE DOCUMENTS
 
-      db.collection("users")
-        .updateOne(
-          { _id: new ObjectID("5f20767620b3bf4828aa3ab3") },
-          {
-            $inc: {
-              age: 1,
-            },
-          }
-        )
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    db.collection("users")
+      .updateOne(
+        { _id: new ObjectID("5f20767620b3bf4828aa3ab3") },
+        {
+          $inc: {
+            age: 1,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-      db.collection("tasks")
-        .updateMany({ completed: true }, { $set: { completed: false } })
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    db.collection("tasks")
+      .updateMany({ completed: true }, { $set: { completed: false } })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
     //DELETE COLLECTIONS
 
