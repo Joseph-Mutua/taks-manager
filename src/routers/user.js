158b4b91409119a3accb97ed4084151da93805c7
Joseph Mutua <Mutuaj793@gmail.com>
2020-08-08 12:19:02 +0300

    Initial setup for logging in users

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,6 +15,18 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 //READ USERS
 
 router.get("/users", async (req, res) => {
